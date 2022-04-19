187942f6c9bc14de862ae826091189e11287def7
Oscar Cortez <oacortez98@gmail.com>
2022-04-18 20:53:50 -0600

    Add api file to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
-import api from './apiCalls'
-import {react, useState, useEffect} from "react"; 
+import api from '../src/Components/apiCalls'
+import {useState, useEffect} from "react"; 
+import NavBar from './Components/navBar'
 
 function App() {
 const [result, setResult] = useState()
@@ -13,22 +13,9 @@ useEffect(() => {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
     </div>
-  );
+  )
 }
 
 export default App;
