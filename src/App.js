6190809360785bd50d950b12ac3c6ae88c4fd822
Oscar Cortez <oacortez98@gmail.com>
2022-04-19 19:35:05 -0600

    Remove state and useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import {useState, useEffect} from 'react'; 
-import api from '../src/Components/apiCalls';
 import NavBar from './Components/navBar';
 import ArticleContainer from './Components/articlesContainer';
 import { Route, Routes} from 'react-router-dom';
 import './Styles/App.css';
 
-
-function App() {
-const [result, setResult] = useState()
-
-useEffect(() => {
-  api()
-    .then(data => console.log(data.results))
-}, [])
+const App = () => {
 
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route exact path='/' element={<ArticleContainer />}/>
+        <Route exact path='/' element={<ArticleContainer/>}/>
       </Routes>
     </div>
   )
