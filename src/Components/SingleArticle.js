67596174e1fcc8da7deba53fc926b1117f101276
Oscar Cortez <oacortez98@gmail.com>
2022-04-20 14:41:15 -0600

    Add home link to redirect user back to home page

--- src/Components/SingleArticle.js
+++ src/Components/SingleArticle.js
@@ -1,6 +1,6 @@
 import api from './apiCalls';
 import {useState, useEffect} from 'react'; 
-import { useParams } from 'react-router-dom';
+import { useParams, Link} from 'react-router-dom';
 import '../Styles/SingleArticle.css';
 
 const SingleArticle = () => {
@@ -18,12 +18,14 @@ const SingleArticle = () => {
     })
   }, [])
 
+
   return (
     <div className="single-card-container">
       <h1>{article.title}</h1>
       <img src={ article.multimedia && article.multimedia[0].url}  className="single-card-image" alt={article.abstract}/>
       <h2>{article.abstract}</h2>
       <a href={article.url}>Click here to read full article</a>
+      <Link className="link" to="/">Return Home</Link>
     </div>
   )
 }
