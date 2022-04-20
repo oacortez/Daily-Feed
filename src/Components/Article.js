8e64f4bc7e24bb534f290dd95089fe3859c240b1
Oscar Cortez <oacortez98@gmail.com>
2022-04-20 14:21:51 -0600

    Add link path to single card

--- src/Components/Article.js
+++ src/Components/Article.js
@@ -1,11 +1,16 @@
-import '../Styles/Article.css'
+import '../Styles/Article.css';
+import { Link } from 'react-router-dom';
 
-const Article = ({title, author, image}) => {
+const Article = ({title, author, image, id}) => {
   return (
     <div className='cards'>
+      {console.log(id)}
       <h1>{title}</h1>
       <img src={image} alt='News article image'/>
       <h2>{author}</h2>
+      <Link to={`/${id}`}>
+        <button>View article</button>
+      </Link>
     </div>
   )
 }
