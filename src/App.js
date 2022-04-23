fbd374886f6185745af06d372cfd87e13dfe8b19
Oscar Cortez <oacortez98@gmail.com>
2022-04-23 13:41:05 -0600

    Remove spaces

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import NavBar from './Components/navBar';
-import ArticleContainer from './Components/articlesContainer';
-import SingleArticle from './Components/SingleArticle'
 import { Route, Routes} from 'react-router-dom';
 import './Styles/App.css';
-import api from './Components/apiCalls'
-import {useState, useEffect} from 'react';
+import ArticleContainer from './Components/articlesContainer';
+import SingleArticle from './Components/SingleArticle'
 
 const App = () => {
 
-// const [results, setResults] = useState([]);
-// const [ currentArticles, setCurrentArticles ] = useState([]);
-
-//   useEffect(() => {
-//     api()
-//     .then(data => {
-//       setResults(data.results)
-//       setCurrentArticles(data.results)
-//     })
-//   }, [])
-  // console.log(results[0]);
-
   return (
     <div className="App">
-      {/* <NavBar /> */}
       <Routes>
         <Route exact path='/' element={<ArticleContainer/>}/>
         <Route exact path='/:id' element={<SingleArticle />}/>
