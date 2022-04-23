e4335ed594f62066314ca8e3d1763c6a8dd2ffe2
Oscar Cortez <oacortez98@gmail.com>
2022-04-23 10:54:35 -0600

    Add filter feature

--- src/Components/navBar.js
+++ src/Components/navBar.js
@@ -1,7 +1,36 @@
 import { NavLink } from 'react-router-dom';
 import '../Styles/navBar.css';
+import React, { useState, useEffect } from 'react'
 
-const NavBar = () => {
+const NavBar = ({news, setFilter}) => {
+
+const [dropdown, setDropdown] = useState([]);
+
+useEffect(() => {
+  const sections = [];
+  news.forEach(article => {
+    !sections.includes(article.section) && sections.push(article.section)
+  })
+  setDropdown(sections)
+}, [news])
+
+const options = () => {
+  return dropdown.map(tag => {
+    return (
+      <option key={tag} value={tag}>{tag}</option>
+    )
+  })
+}
+
+const filterArticles = (event) => {
+  if(event.target.value === "all") {
+    setFilter(news)
+  } else {
+    setFilter(news.filter(article => {
+      return article.section === event.target.value
+    }))
+  }
+}
 
   return (
     <nav>
@@ -10,6 +39,14 @@ const NavBar = () => {
           The Daily Feed 
         </button>
       </NavLink>
+      <select
+        className="section-select"
+        defaultValue="all"
+        onChange={event => filterArticles(event)
+      }>
+        <option value="all">All Stories</option>
+        { options() }
+      </select>
     </nav>
   )
 }
