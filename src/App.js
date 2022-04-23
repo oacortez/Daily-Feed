9d83088a8d0f46400d5fae8af74fc720de5305d2
Oscar Cortez <oacortez98@gmail.com>
2022-04-23 10:23:04 -0600

    Remove Navbar component from app.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import ArticleContainer from './Components/articlesContainer';
 import SingleArticle from './Components/SingleArticle'
 import { Route, Routes} from 'react-router-dom';
 import './Styles/App.css';
+import api from './Components/apiCalls'
+import {useState, useEffect} from 'react';
 
 const App = () => {
 
+// const [results, setResults] = useState([]);
+// const [ currentArticles, setCurrentArticles ] = useState([]);
+
+//   useEffect(() => {
+//     api()
+//     .then(data => {
+//       setResults(data.results)
+//       setCurrentArticles(data.results)
+//     })
+//   }, [])
+  // console.log(results[0]);
+
   return (
     <div className="App">
-      <NavBar />
+      {/* <NavBar /> */}
       <Routes>
         <Route exact path='/' element={<ArticleContainer/>}/>
         <Route exact path='/:id' element={<SingleArticle />}/>
